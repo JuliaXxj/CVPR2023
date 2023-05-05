f165cdc43211a153c82ba8fbfd721064f25850b9
Xiaojie Xu <xiaojie.xu@mila.quebec>
2023-05-05 12:31:03 -0400

    add dummy model

--- FCN/models.py
+++ FCN/models.py
@@ -185,6 +185,43 @@ class FeedforwardNeuralNetModel(BaseNet):
     def model_savename(self):
         return "FFN" + datetime.now().strftime("%H-%M-%S")
 
+
+class DummyFCN(BaseNet):
+    def __init__(self, input_dim, hidden_dim, output_dim, bias=True):
+        super(FeedforwardNeuralNetModel, self).__init__()
+
+        # Linear function
+        self.fc1 = nn.Linear(input_dim, 10, bias=bias)
+        self.fc2 = nn.Linear(10, 10, bias=bias)
+        self.fc3 = nn.Linear(10, output_dim, bias=bias)
+
+    def register_log(self, detach=True):
+        self.reset_hooks()
+        # first layer should not make any difference?
+        self.hooks.append(self.fc1.register_forward_hook(get_activation('fc1', self.tensor_log, detach)))
+        self.hooks.append(self.fc2.register_forward_hook(get_activation('fc2', self.tensor_log, detach)))
+
+        self.hooks.append(
+            self.fc3.register_forward_hook(get_activation('fc3', self.tensor_log, detach, is_lastlayer=True)))
+
+    def register_gradient(self, detach=True):
+        self.reset_bw_hooks()
+        # first layer should not make any difference?
+        self.bw_hooks.append(self.fc1.register_backward_hook(get_gradient('fc1', self.gradient_log, detach)))
+        self.bw_hooks.append(self.fc2.register_backward_hook(get_gradient('fc2', self.gradient_log, detach)))
+        self.bw_hooks.append(self.fc3.register_backward_hook(get_gradient('fc3', self.gradient_log, detach)))
+
+
+    def forward(self, x):
+        out = F.relu(self.fc1(x.view(-1, 28 * 28)))
+        out = self.fc2(out)
+        out = self.fc3(out)
+        #    out = F.log_softmax(out, dim=1)
+        return out
+
+    def model_savename(self):
+        return "DummyFCN" + datetime.now().strftime("%H-%M-%S")
+
 class VNN_FFN_RELU_2(BaseNet):
     def __init__(self, input_dim, hidden_dim, output_dim):
         super(VNN_FFN_RELU_2, self).__init__()
